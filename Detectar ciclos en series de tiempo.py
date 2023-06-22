ce01cf9418aa498268d437efd17b4b18a1607f6d
Roger Fuentes <fuentesroger65@gmail.com>
2023-06-22 00:54:46 -0400

    Probando con un Lotka-Volterra

--- Detectar ciclos en series de tiempo.py
+++ Detectar ciclos en series de tiempo.py
@@ -1,4 +1,30 @@
 import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+# Define el sistema de ecuaciones diferenciales
+def lotka_volterra(t, y, a, b, c, d):
+    x, y = y
+    dxdt = a*x - b*x*y
+    dydt = c*x*y - d*y
+    return [dxdt, dydt]
+
+# Define los parámetros del modelo
+a = 1.5
+b = 1
+c = 3
+d = 1
+
+# Define los puntos iniciales para la integración
+t_span = [0, 20]
+y0 = [10, 5]
+
+# Integra el sistema de ecuaciones diferenciales
+sol = solve_ivp(lotka_volterra, t_span, y0, args=(a, b, c, d), dense_output=True)
+
+
+
+
 def detectar_ciclo(t, y):
     # Calcular la Transformada Rápida de Fourier
     fft_result = np.fft.fft(y)
@@ -17,8 +43,8 @@ def detectar_ciclo(t, y):
     return periodo
 
 # Definir la serie de tiempo
-t = np.linspace(0, 4 * np.pi, 500) # 500 puntos entre 0 y 4*pi
-y = np.sin(t)
+t = np.linspace(0, 20, 1000)
+y = sol.sol(t)
 
 # Detectar el ciclo
 periodo = detectar_ciclo(t, y)

8ef6a0a7b0e9912fa58c4b096a1178849ff31cca
Roger Fuentes <fuentesroger65@gmail.com>
2023-06-24 20:00:18 -0400

    Aumentando la precision

--- Detectar ciclos en series de tiempo.py
+++ Detectar ciclos en series de tiempo.py
@@ -1,51 +1,33 @@
+
 import numpy as np
-from scipy.integrate import solve_ivp
-import matplotlib.pyplot as plt
 
-# Define el sistema de ecuaciones diferenciales
-def lotka_volterra(t, y, a, b, c, d):
-    x, y = y
-    dxdt = a*x - b*x*y
-    dydt = c*x*y - d*y
-    return [dxdt, dydt]
-
-# Define los parámetros del modelo
-a = 1.5
-b = 1
-c = 3
-d = 1
-
-# Define los puntos iniciales para la integración
-t_span = [0, 20]
-y0 = [10, 5]
-
-# Integra el sistema de ecuaciones diferenciales
-sol = solve_ivp(lotka_volterra, t_span, y0, args=(a, b, c, d), dense_output=True)
-
-
-
-
-def detectar_ciclo(t, y):
+def detectar_ciclo(t, y, tolerancia=1e-10):
     # Calcular la Transformada Rápida de Fourier
     fft_result = np.fft.fft(y)
-    freqs = np.fft.fftfreq(len(y), d=t[1]-t[0])  # ahora tomando en cuenta el espaciado de tiempo
+    freqs = np.fft.fftfreq(len(y), d=t[1]-t[0])
     
     # Ignorar la mitad de los resultados debido a la simetría compleja conjugada
     fft_result = fft_result[:len(fft_result)//2]
     freqs = freqs[:len(freqs)//2]
     
     # Encuentra la frecuencia con la amplitud más alta
-    indice_frecuencia_maxima = np.argmax(np.abs(fft_result))
+    amplitudes = np.abs(fft_result)
+    indice_frecuencia_maxima = np.argmax(amplitudes)
     frecuencia_maxima = freqs[indice_frecuencia_maxima]
     
     # El período es el inverso de la frecuencia
-    periodo = 1.0 / frecuencia_maxima if frecuencia_maxima != 0 else None
+    if np.abs(frecuencia_maxima) > tolerancia: 
+        periodo = 1.0 / frecuencia_maxima
+    else:
+        print("No se puede determinar el período, la frecuencia máxima es demasiado pequeña.")
+        periodo = None
     return periodo
 
 # Definir la serie de tiempo
-t = np.linspace(0, 20, 1000)
-y = sol.sol(t)
+t = np.linspace(0, 4 * np.pi, 5000) # 5000 puntos entre 0 y 4*pi
+y = np.sin(t)
 
 # Detectar el ciclo
 periodo = detectar_ciclo(t, y)
 print(f'Periodo detectado: {periodo}')
+print(2* np.pi)
